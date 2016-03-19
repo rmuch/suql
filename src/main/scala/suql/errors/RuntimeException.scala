22de93189352b338876b52b7e65d90ab0210430c
Rupert Muchembled <rupert@rmuch.net>
2016-03-19 12:54:54 +0000

    Extend exception type hierarchy

--- src/main/scala/suql/errors/RuntimeException.scala
+++ src/main/scala/suql/errors/RuntimeException.scala
@@ -1,7 +1,13 @@
 package suql.errors
 
-class SuqlRuntimeException(message: String) extends Exception
-class SuqlRuntimeTypeException(message: String) extends Exception
-class SuqlRuntimeArgumentException(message: String) extends SuqlRuntimeException(message)
-class SuqlQueryException(message: String) extends Exception
-class SuqlRuntimeError(message: String) extends Error
+trait SuqlThrowable
+trait SuqlError extends SuqlThrowable
+trait SuqlException extends SuqlThrowable
+
+class SuqlRuntimeException(message: String) extends Exception with SuqlException
+class SuqlRuntimeTypeException(message: String) extends Exception with SuqlException
+class SuqlRuntimeArgumentException(message: String) extends Exception with SuqlException
+class SuqlQueryException(message: String) extends Exception with SuqlException
+
+class SuqlRuntimeError(message: String) extends Error with SuqlError
+class SuqlNotImplementedError(message: String) extends Error with SuqlError
